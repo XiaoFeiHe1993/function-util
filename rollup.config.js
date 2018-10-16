3c2a4b49eff305f2c3193a4ca0abca5245f96f80
JackFei <1712776213@qq.com>
2018-10-16 23:55:35 +0800

    chore:添加rollup压缩配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,36 @@
-export default {
+import babel from 'rollup-plugin-babel';
+import {uglify} from 'rollup-plugin-uglify';
+import replace from 'rollup-plugin-replace';
+
+var env = process.env.NODE_ENV;
+
+var config = {
     input: 'index.js',
     output: {
         file: 'bundle.js',
         format: 'cjs'
-    }
+    },
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ]
 };
+
+if (env === 'production') {
+    config.plugins.push(
+        uglify({
+            compress: {
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                warnings: false
+            }
+        })
+    )
+}
+
+export default config;

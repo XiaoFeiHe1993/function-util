d864d18c995af894d2184de6b15323b83bf3fa88
JackFei <1712776213@qq.com>
2019-05-22 23:36:23 +0800

    feat: 增加密码强度判断

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import {getFirstDate, getFinalDate, formatNumber, getUUID} from './lib/timeUtil'
 import {isUrl, isEmail} from './lib/isUtil';
 import {formatBytes} from './lib/fileUtil';
 import {getParameterByName, setCookie, getCookie} from './lib/urlUtil';
-import {fixedBody, looseBody, showText, removeFormEmpty, htmlEncode, htmlDecode} from './lib/otherUtil';
+import {fixedBody, looseBody, showText, removeFormEmpty, htmlEncode, htmlDecode, checkPwd} from './lib/otherUtil';
 import {rem} from './lib/remUtil';
 
 module.exports = {
@@ -28,5 +28,7 @@ module.exports = {
     rem,
 
     htmlEncode,
-    htmlDecode
+    htmlDecode,
+
+    checkPwd
 };
